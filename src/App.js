9c0fb8aea952aafea392b1a05b54021c2d77e660
vanbich <vannguyen300998@gmail.com>
2020-07-25 22:57:03 +0700

    final commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import {BrowserRouter} from "react-router-dom";
 import Routes from './Routes';
+import './index.css'
 
 function App() {
+    const [abc, setAbc] = React.useState('');
+
+    const handle = (e)=>{
+        console.log("e", e.target.value);
+        setAbc(e.target.value);
+    };
   return (
       <BrowserRouter>
+          <select className="abc" onChange={handle} value={abc}>
+              <option value={"abc"}>abc</option>
+              <option value={"def"}>def</option></select>
           <Routes/>
       </BrowserRouter>
   );
